281425838d7080aaafde0d7e81820a25ed591871
aeprodigy <mikeapple056@gmail.com>
2024-01-22 09:38:10 +0200

    load screen fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Pages/HomePage";
+import { useState } from "react";
 
 
 const App = () => {
+   const [loading, setLoading] = useState(true);
+   const spinner = document.getElementById("spinner");
+   if (spinner) {
+     setTimeout(() => {
+       spinner.style.display = "none";
+       spinner.style.transition = "2s";
+       setLoading(false);
+     }, 2000);
+   }
   return (
+    !loading && (
     <div className=''>
       <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
       </Routes>
     </div>
+    )
   );
 }
 
