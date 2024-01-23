595ae8292df09ea6fb6f240858edff8f9ea29b82
aeprodigy <mikeapple056@gmail.com>
2024-01-23 17:39:46 +0200

    logo fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,11 +4,13 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 w-full z-[100] absolute">
       <h1 className="text-blue-600 font-bold text-2xl cursor-pointer">
-        BLUM<span className="text-gray-400">HOUSE</span>
+        NEP<span className="text-gray-400">TUNE🔵</span>
       </h1>
       <div>
-        <button className=" py-2 px-6 rounded  text-white pr-4">Sign In</button>
-        <button className="bg-blue-600 py-2 px-6 rounded text-white cursor pointer">
+        <button className=" py-2 px-6 rounded border border-gray-300  text-white pr-4">
+          Sign In
+        </button>
+        <button className="bg-blue-600 ml-2 py-2 px-6 rounded text-white cursor pointer">
           Sign Up
         </button>
       </div>
